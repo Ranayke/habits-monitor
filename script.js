3b0c0d27ef528aa671a8c873cf366ee0f9023bc5
Ranayke Boni Ferreira <ranayke2009@gmail.com>
2023-01-19 22:24:59 -0300

    feat(js): implemented add and save interactions

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  pedal: ["01-01", "01-02", "01-06"],
-  water: ['01-03', "01-05"],
-  food: ["01-04"]
-}
+button.addEventListener("click", () => { //add
+  const today = new Date().toLocaleDateString('pt-br').slice(0,-5)
+  const dayExists = nlwSetup.dayExists(today)
+  if (dayExists) {
+    alert("Dia já incluso")
+    return
+  }
+  nlwSetup.addDay(today)
+})
 
+form.addEventListener('change', () => { //save
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+})
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
